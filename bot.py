1a6a6a33d73ba20e707725414906cd3415cad546
Benjamin Siege <ben.siege@gmail.com>
2020-02-28 23:27:48 -0500

    add info retrieval

--- bot.py
+++ bot.py
@@ -41,34 +41,53 @@ async def cmess(ctx,*args):
 @client.command()
 async def reloadC(ctx,arg):
     client.reload_extension(arg)
-
+    
 @client.command()
 async def happiness(ctx):
     await ctx.send("`Remember, happiness is mandatory`")
-if os.path.exists("auth.txt"):
-    with open("auth.txt",'r') as authF:
-        auth = authF.read()
-else:
-    auth =os.environ['AUTH_KEY']
-    print(auth)
 
-#make a more modular function -> pass name of column to mod into method
+
 @client.command()
 async def mod_treason(ctx,name,value="1"):
+    await modify_table(ctx,name,"user_treason",value)
+
+@client.command()
+async def mod_xp(ctx,name,value):
+    await modify_table(ctx,name,"user_xpp",value)
+
+@client.command()
+async def mod_clearence(ctx,name,value):
+    await modify_table(ctx,name,"user_clearence",value)
+
+@client.command()
+async def pinfo(ctx, name=""):
+    if name=="":
+        name = ctx.message.author.name
     cur = conn.cursor()
-    cur.execute("select user_name,user_treason from users where user_name=\'"+name+"\';")
+    cur.execute("select user_name,user_xpp,user_treason,user_clearence from users where user_name='"+name+"';")
+    column_names = [desc[0] for desc in cur.description]
+    line1 = "`{:<15}{:<15}{:<15}{:<15}`".format(*column_names)
+    out = cur.fetchone()
+    if out:
+        line2 =line1+"\n"+ "`{:<15}{:<15}{:<15}{:<15}`".format(*out)
+        await ctx.send(line2)
+    else:
+        await ctx.send("**NO CLONE BY THAT NAME**")
+    
+async def modify_table(ctx,name,column,value="1"):
+    cur = conn.cursor()
+    cur.execute("select user_name,"+column+" from users where user_name=\'"+name+"\';")
     user = cur.fetchone()
     try:
         value = int(value)
     except:
         value = 1
     if user:
-        await ctx.send(f"**Treason of {user[0]} goes from {user[1]} to {user[1]+value}**")
-        cur.execute("Update users set user_treason=(%s) where user_name=(%s);",(user[1]+value,user[0]))
+        await ctx.send(f"**{column} of {user[0]} goes from {user[1]} to {user[1]+value}**")
+        cur.execute("Update users set "+column+"=(%s) where user_name=(%s);",(user[1]+value,user[0]))
         conn.commit()
         print_usr_data(cur)
-    
-    
+
 def initializeDB(conn,membs):
     cur = conn.cursor()
     [cur.execute("INSERT INTO users(user_dis_id,user_name) VALUES (%s,%s) ON CONFLICT DO NOTHING;",
@@ -84,6 +103,14 @@ def print_usr_data(cur):
     print("{:<10}{:<25}{:<20}{:<20}{:<20}{:<20}".format(*column_names))
     [print("{:<10}{:<25}{:<20}{:<20}{:<20}{:<20}".format(*x)) for x in cur.fetchall()]
     
+
+if os.path.exists("auth.txt"):
+    with open("auth.txt",'r') as authF:
+        auth = authF.read()
+else:
+    auth =os.environ['AUTH_KEY']
+    print(auth)
+
 def setup(client):
     client.run(auth)
     
