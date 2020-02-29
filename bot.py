e641cc89f7fc5799abd265171ec9a511fb913585
Benjamin Siege <ben.siege@gmail.com>
2020-02-28 20:28:28 -0500

    add database functionality, increace treason stars

--- bot.py
+++ bot.py
@@ -1,5 +1,11 @@
 import discord, os
 from discord.ext import commands
+import os
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 client = commands.Bot(command_prefix='FC_', description='Your Friend')
 auth = ""
 @client.event
@@ -8,12 +14,15 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+    for g in client.guilds:
+        initializeDB(conn,g.members)
 
 @client.command()
 async def close(ctx,*args):
    usr = ctx.message.author
    if usr.name == "siegeerson":
        await client.close()
+
 
 @client.command()
 async def cmess(ctx,*args):
@@ -27,6 +36,7 @@ async def cmess(ctx,*args):
     except Exception as e:
         print(e)
         await ctx.send("ERROR COMPUTER UNAVAILABLE")
+
 #TODO: extensions/commands can be stored in seperate file
 @client.command()
 async def reloadC(ctx,arg):
@@ -41,7 +51,36 @@ if os.path.exists("auth.txt"):
 else:
     auth =os.environ['AUTH_KEY']
     print(auth)
+
+@client.command()
+async def inc_treason(ctx,name):
+    cur = conn.cursor()
+    cur.execute("select user_name,user_treason from users where user_name=\'"+name+"\';")
+    user = cur.fetchone()
+    if user:
+        await ctx.send(f"**Treason of {user[0]} goes from {user[1]} to {user[1]+1}**")
+        cur.execute("Update users set user_treason=(%s) where user_name=(%s);",(user[1]+1,user[0]))
+        conn.commit()
+        print_usr_data(cur)
+    
+    
+def initializeDB(conn,membs):
+    cur = conn.cursor()
+    [cur.execute("INSERT INTO users(user_dis_id,user_name) VALUES (%s,%s) ON CONFLICT DO NOTHING;",
+                 (usr.id,usr.name))
+     for usr in membs]
+    conn.commit()
+    print_usr_data(cur)
+
+    
+def print_usr_data(cur):
+    cur.execute("select * from users;")
+    column_names = [desc[0] for desc in cur.description]
+    print("{:<10}{:<25}{:<20}{:<20}{:<20}{:<20}".format(*column_names))
+    [print("{:<10}{:<25}{:<20}{:<20}{:<20}{:<20}".format(*x)) for x in cur.fetchall()]
+    
 def setup(client):
     client.run(auth)
+    
     
 setup(client)
