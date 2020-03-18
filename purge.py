223f2561613f5faff2757bf994495387892f1418
Benjamin Siege <ben.siege@gmail.com>
2020-03-18 15:13:56 -0400

    cleanup database access

--- purge.py
+++ purge.py
@@ -4,7 +4,7 @@ if os.path.exists("auth.txt"):
     with open("auth.txt",'r') as authF:
         auth = authF.read()
 
-ch = 683041465659818024
+ch = 689247879487160372
 client = commands.Bot(command_prefix='FC_', description='Your Friend')
 @client.event
 async def on_ready():
--- user_commands.py
+++ user_commands.py
@@ -1,6 +1,7 @@
-import discord, os, psycopg2,random, asyncio
+import discord, os, psycopg2,random, asyncio, typing
+from psycopg2 import sql
 from discord.ext import commands
-
+#TODO make database update based on a lambda current value -> output
 class Users(commands.Cog):
     def __init__(self, bot,db_connection):
         self.bot = bot
@@ -20,30 +21,30 @@ class Users(commands.Cog):
             self.print_usr_data(cur)
         
     @commands.command(hidden=True)
-    async def mod_clone(self,ctx,name):
+    async def mod_clone(self,ctx,name : discord.User):
         await self.modify_table(ctx,name,"user_clone",1)
 
     @commands.command(hidden=True)
-    async def mod_treason(self,ctx,name,value="1"):
+    async def mod_treason(self,ctx,name : discord.User,value="1"):
         await self.modify_table(ctx,name,"user_treason",value)
 
     @commands.command(hidden=True)
-    async def mod_xp(self,ctx,name,value):
+    async def mod_xp(self,ctx,name : discord.User ,value):
         await self.modify_table(ctx,name,"user_xpp",value)
 
     @commands.command(hidden=True)
-    async def mod_clearence(self,ctx,name,value):
+    async def mod_clearence(self,ctx,name : discord.User,value):
         await self.modify_table(ctx,name,"user_clearence",value)
 
     @commands.command(brief="See stats about yourself")
-    async def pinfo(self,ctx, name=""):
-        if name=="":
-            name = ctx.message.author.name
-            cur = self.db_connection.cursor()
-            cur.execute("select user_name,user_xpp,user_treason,user_clearence,user_clone from users where user_name='"+name+"';")
-            column_names = [desc[0] for desc in cur.description]
-            line1 = "`{:<15}{:<15}{:<15}{:<15}{:<15}`".format(*column_names)
-            out = cur.fetchone()
+    async def pinfo(self,ctx,name: typing.Optional[discord.User]):
+        if not name:
+            name = ctx.message.author
+        cur = self.db_connection.cursor()
+        cur.execute("select user_name,user_xpp,user_treason,user_clearence,user_clone from users where user_dis_id=%s;",[str(name.id)])
+        column_names = [desc[0] for desc in cur.description]
+        line1 = "`{:<15}{:<15}{:<15}{:<15}{:<15}`".format(*column_names)
+        out = cur.fetchone()
         if out:
             line2 =line1+"\n"+ "`{:<15}{:<15}{:<15}{:<15}{:<15}`".format(*out)
             await ctx.send(line2)
@@ -53,7 +54,7 @@ class Users(commands.Cog):
 
     async def modify_table(self,ctx,name,column,value="1"):
         cur = self.db_connection.cursor()
-        cur.execute("select user_name,"+column+" from users where user_name=\'"+name+"\';")
+        cur.execute(sql.SQL("select user_name,{column} from users where user_dis_id=%s;").format(column=sql.Identifier(column)),[str(name.id)])
         user = cur.fetchone()
         try:
             value = int(value)
@@ -61,7 +62,7 @@ class Users(commands.Cog):
             value = 1
         if user:
             await ctx.send(f"**{column} of {user[0]} goes from {user[1]} to {user[1]+value}**")
-            cur.execute("Update users set "+column+"=(%s) where user_name=(%s);",(user[1]+value,user[0]))
+            cur.execute(sql.SQL("Update users set {}=(%s) where user_name=(%s);").format(sql.Identifier(column)),[user[1]+value,user[0]])
             self.db_connection.commit()
             self.print_usr_data(cur)
 
